af74aa337e3be070f365cf8ab78c7086948b3425
ducva <ducvuanh.79@gmail.com>
2024-04-05 16:17:27 +0700

    feat(mason): enable mason customize, add graphql lspconfig

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -13,6 +11,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
         -- add more arguments for adding more language servers
+        "graphql",
       })
     end,
   },
